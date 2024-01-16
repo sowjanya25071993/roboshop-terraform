903ebb4ee1cf259ed5e0d18a8237dd8d81ec9bc6
sowji <jujjurisowjanya@gmail.com>
2024-01-16 17:49:21 +0530

    new

---
+++ 01-vpc/main.tf
@@ -0,0 +1,12 @@
+ module "roboshop" {
+    source = "git::https://github.com/daws-76s/terraform-aws-vpc.git?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    common_tags = var.common_tags
+    vpc_tags = var.vpc_tags
+    # public subnet
+    public_subnets_cidr = var.public_subnets_cidr
+    private_subnets_cidr = var.private_subnets_cidr
+    database_subnets_cidr = var.database_subnets_cidr
+    is_peering_required = var.is_peering_required
+}
